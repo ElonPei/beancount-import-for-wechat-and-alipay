b44c00053d2301873d409265ebbb606316302b99
Peiel <peielcn@gmail.com>
2023-02-14 15:33:41 +0800

    持久化标签和备注逻辑优化

--- import_manager/beans_loader.py
+++ import_manager/beans_loader.py
@@ -1,10 +1,19 @@
-import glob
+import fnmatch
 import os
 import re
 
 import pandas as pd
 
-bean_path = '/Users/peiel/Library/Mobile Documents/com~apple~CloudDocs/Personal/财务/beancount'
+
+def find_all_auto_bean_file():
+    beans_paths = []
+    bean_path = '/Users/peiel/Library/Mobile Documents/com~apple~CloudDocs/Personal/财务/beancount'
+    # 遍历目录树查找所有符合条件的文件
+    for root, dirs, files in os.walk(bean_path):
+        for filename in fnmatch.filter(files, '*auto.bean'):
+            # 输出文件名或者执行其他操作
+            beans_paths.append(os.path.join(root, filename))
+    return beans_paths
 
 
 def starts_with_date(string):
@@ -14,6 +23,7 @@ def starts_with_date(string):
 
 
 def extract_tags(transaction_line):
+    transaction_line = re.sub(r'\"[^\"]+\"', '', transaction_line)
     tags = []
     fields = transaction_line.split(" ")
     for field in fields:
@@ -24,17 +34,15 @@ def extract_tags(transaction_line):
 
 def load_remark_info():
     lines = []
-    # todo 读取所有导出的文件
-    for f in glob.glob(os.path.join(bean_path, 'wechat.bean')):
+    for f in find_all_auto_bean_file():
         with open(f, "r") as file:
-            lines = file.readlines()
+            lines = lines + file.readlines()
     lines_list = [line.strip() for line in lines if line.startswith("\tid:") or line.startswith("\tremark:") or starts_with_date(line)]
     lines_list = [line.replace('\t', '') for line in lines_list]
     lines_list = [line.replace('\n', '') for line in lines_list]
-    lines_list = [line.replace('"', '') for line in lines_list]
 
-    id_list = [line.split(':')[1].strip() for line in lines_list if line.startswith('id:')]
-    remark_list = [line.split(':')[1].strip() for line in lines_list if line.startswith('remark:')]
+    id_list = [line.split(':')[1].strip().replace('"', '') for line in lines_list if line.startswith('id:')]
+    remark_list = [line.split(':')[1].strip().replace('"', '') for line in lines_list if line.startswith('remark:')]
     tags_list = [extract_tags(line) for line in lines_list if starts_with_date(line)]
 
     df = pd.DataFrame({
